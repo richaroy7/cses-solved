2803f212405ebaf0ba81035c186311974be85952
Richa Roy <richaroydps@gmail.com>
2022-12-05 21:11:45 +0530

    distinct numbers

---
+++ a.cpp
@@ -0,0 +1,32 @@
+ #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define mod 1000000007
+    #define rep(i,a,n) for(long long i=a;i<n;i++)
+    #define rrep(i,n,k) for(int i=n;i>=k;i--)
+    #define yes cout<<"YES"<<endl;
+    #define no cout<<"NO"<<endl;
+    void solve()
+    {
+       ll n;
+       cin>>n;
+       set<ll>st;
+       rep(i,0,n)
+       {
+          ll x;
+          cin>>x;
+          st.insert(x);
+       }
+       cout<<st.size()<<endl;
+    }
+    int main() {
+     
+       ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+        ll test=1;
+        //cin>>test;
+        while(test--)
+        {
+         solve();
+        }
+        return 0;
+    }
