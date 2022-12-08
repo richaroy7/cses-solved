4f891fd29a464d5613c425670d70637cfce08b17
Richa Roy <richaroydps@gmail.com>
2022-12-08 23:19:22 +0530

    missing number

---
+++ missingnumber.cpp
@@ -0,0 +1,40 @@
+ #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define mod 1000000007
+    #define rep(i,a,n) for(long long i=a;i<n;i++)
+    #define rrep(i,n,k) for(int i=n;i>=k;i--)
+    #define yes cout<<"YES"<<endl;
+    #define no cout<<"NO"<<endl;
+    void solve()
+    {
+        ll n;
+        cin>>n;
+        ll a[n+1]={0};
+        for(ll i=1;i<n;i++)
+        {
+            ll x;
+            cin>>x;
+            a[x]=1;
+        }
+        for(ll i=1;i<=n;i++)
+        {
+            if(a[i]==0)
+            {
+                cout<<i<<endl;
+                break;
+            }
+        }
+        
+    }
+    int main() {
+     
+       ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+        ll test=1;
+       // cin>>test;
+        while(test--)
+        {
+         solve();
+        }
+        return 0;
+    }
