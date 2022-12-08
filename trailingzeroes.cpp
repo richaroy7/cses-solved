bae5bc0d09e3e37acb15d267be29cf9dbdb5b630
Richa Roy <richaroydps@gmail.com>
2022-12-08 23:51:23 +0530

    trailing zeroes

---
+++ trailingzeroes.cpp
@@ -0,0 +1,30 @@
+ #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define mod 1000000007
+    #define rep(i,a,n) for(long long i=a;i<n;i++)
+    #define rrep(i,n,k) for(int i=n;i>=k;i--)
+    #define yes cout<<"YES"<<endl;
+    #define no cout<<"NO"<<endl;
+    void solve()
+    {
+        ll n;
+        cin>>n;
+        
+        ll ans=0;
+        for(ll i=5;i<=n;i=i*5)
+        ans+=(n/i);
+        
+        cout<<(ans)<<endl;
+    }
+    int main() {
+     
+       ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+        ll test=1;
+        //cin>>test;
+        while(test--)
+        {
+         solve();
+        }
+        return 0;
+    }
