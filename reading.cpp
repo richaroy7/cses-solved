1498ee337285d7ae70d246185546b8cbdbb6bc13
Richa Roy <richaroydps@gmail.com>
2022-12-22 01:14:02 +0530

    reading  books

---
+++ reading.cpp
@@ -0,0 +1,36 @@
+ #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define mod 1000000007
+    #define rep(i,a,n) for(long long i=a;i<n;i++)
+    #define rrep(i,n,k) for(int i=n;i>=k;i--)
+    #define yes cout<<"YES"<<endl;
+    #define no cout<<"NO"<<endl;
+    void solve()
+    {
+        ll n;
+        cin>>n;
+        ll a[n];
+        ll sum=0;
+        rep(i,0,n)
+        {
+            cin>>a[i];
+            sum+=a[i];
+            
+            
+        }
+        sort(a,a+n);
+        cout<<max(sum,(2*a[n-1]))<<endl;
+        
+    }
+    int main() {
+     
+       ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+        ll test=1;
+        //cin>>test;
+        while(test--)
+        {
+         solve();
+        }
+        return 0;
+    }
